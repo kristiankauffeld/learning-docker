56a1d48042a06853a59f95a573e61bada1b3ade6
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-09-04 09:24:17 +0200

    rabbit consumer working, but interferes with hot reloading

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ simple proof of concept (PoC) microservice application that utilizes docker comp
 ### Docker Compose Startup Order
 
 In order to control the order of service startup, I've used the `healthcheck` parameter in the docker-compose file as recommended in this post: [devops.stackexchange] (https://devops.stackexchange.com/questions/12092/docker-compose-healthcheck-for-rabbitmq). This seems to be a better approach at least for the FastAPI service, but the NodeJS service uses `wait-port` package.
+
+### Current Issues
+
+Hot reloading for the "stem-separation" service currently doesn't work. The long-running RabbitMQ consumer seems to interfere with FastAPI's hot-reloading feature.
--- stem-separation/src/app.py
+++ stem-separation/src/app.py
@@ -1,39 +1,70 @@
 from fastapi import FastAPI
+import pika
 import os
 from urllib.parse import urlparse
-import pika
+import threading
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.DEBUG)
+
 
 app = FastAPI(debug=True)
 
+# RabbitMQ server connection parameters
 RABBIT = os.getenv("RABBIT")
-
-if RABBIT is None:
-    raise ValueError("RABBIT environment variable is not set")
 
 parsed_url = urlparse(RABBIT)
 queue_name = "uploaded"
 
-
-
-    # Connect to RabbitMQ
 try:
-    print(f"Parsed RabbitMQ URL: {parsed_url}")
-    print(f"Hostname: {parsed_url.hostname}")
-    print(f"Port: {parsed_url.port}")
-    print(f"Username: {parsed_url.username}")
-    print(f"Password: {parsed_url.password}")
-    connection = pika.BlockingConnection( pika.ConnectionParameters( host=parsed_url.hostname, port=parsed_url.port, credentials=pika.PlainCredentials(parsed_url.username, parsed_url.password), ))
+    # Connect to RabbitMQ
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=parsed_url.hostname,
+            port=parsed_url.port,
+            credentials=pika.PlainCredentials(parsed_url.username, parsed_url.password),
+        )
+    )
 except Exception as e:
-    print(f"Failed to connect to RabbitMQ: {e}")
-    raise
+    print(f"Could not connect to RabbitMQ: {e}")
+    exit(1)
+
+channel = connection.channel()
+
+channel.queue_declare(
+    queue=queue_name, durable=True
+)  
+
+def consume_uploaded_message(ch, method, properties, body):
+    logging.info("Received a message from RabbitMQ.")
+    try:
+        message = json.loads(body.decode('utf-8'))
+        logging.info(f"Received audio for stem separation: {message}")
+    except json.JSONDecodeError:
+        logging.error("Received a message that could not be decoded as JSON.")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=consume_uploaded_message, auto_ack=False
+)
 
 
+def start_rabbitmq_consumer():
+    try:
+        logging.info("Starting to consume messages from RabbitMQ.")
+        channel.start_consuming()
+    except Exception as e:
+        logging.error(f"Error in RabbitMQ consumer: {e}")
 
+
+rabbitmq_thread = threading.Thread(target=start_rabbitmq_consumer)
+rabbitmq_thread.start()
 
 
 @app.get("/")
-def read_root():
-    return {"message": "hello hjhj"}
-
+def index():
+    logging.info("Received a GET request on the / endpoint.")
+    return {"details": "Hello"}
 
